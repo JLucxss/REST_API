24885ddf5587022e948604d96ae5fb2722e68f52
Lucas <jlucas.silvace@gmail.com>
2025-06-10 10:25:38 -0300

    integrating create

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Module({
     imports: [],
     controllers: [UserController],
-    providers: [],
+    providers: [PrismaService],
     exports: []
 })
 export class UserModule {}
